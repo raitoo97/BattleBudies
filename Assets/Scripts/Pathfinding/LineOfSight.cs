acb418d9ac8ce1fbcb3c41bc0c04491065ffe52a
Nicolas <nicolas.migues97@gmail.com>
2025-11-26 12:28:37 -0300

    Clean

--- Assets/Scripts/Pathfinding/LineOfSight.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-public static class LineOfSight
-{
-    public static bool IsOnSight(Vector3 start, Vector3 end)
-    {
-        var dir = end - start;
-        return !Physics.Raycast(start, dir, dir.magnitude, LayerMask.GetMask("Ground"));
-    }
-}
