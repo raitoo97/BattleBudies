105ab05839857508845910aa909b93133ee52848
Nicolas <nicolas.migues97@gmail.com>
2025-11-28 04:09:11 -0300

    FixMovement

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -7,7 +7,7 @@ public class BattleManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("ti");
     }
 
     // Update is called once per frame
