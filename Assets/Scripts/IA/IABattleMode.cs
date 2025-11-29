51a13ec54d743134939ababafa7a825733cad741
Nicolas <nicolas.migues97@gmail.com>
2025-11-28 21:13:12 -0300

    InitEnemigoAtaque

--- Assets/Scripts/IA/IABattleMode.cs
+++ Assets/Scripts/IA/IABattleMode.cs
@@ -7,7 +7,7 @@ public class IABattleMode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Script de ataque del enemigo");
     }
 
     // Update is called once per frame
