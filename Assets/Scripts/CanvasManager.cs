f717cfafda8613463702b25cf07b4999445ebb67
Nicolas <nicolas.migues97@gmail.com>
2025-11-29 19:13:18 -0300

    PreUI

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class CanvasManager : MonoBehaviour
 {
+    //PREUI
     public static CanvasManager instance;
     [Header("Combat")]
     public Button rollButton;
