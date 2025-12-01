9ab7f639ee90fcc59d14b4a39377c9741a745590
Nicolas <nicolas.migues97@gmail.com>
2025-11-30 22:02:46 -0300

    InitAudio

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,18 +1,50 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SoundManager Instance;
+    [SerializeField] private List<AudioSource> sources;
+    [SerializeField] private List<AudioClip> clips;
+    [SerializeField] private int Pool_Size;
+    private void Awake()
     {
-        print("Inicio");
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        CompletePool(Pool_Size);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void CompletePool(int num)
     {
-        
+        for (int i = 0; i < Pool_Size; i++)
+        {
+            AddSoundSource();
+        }
+    }
+    private void AddSoundSource()
+    {
+        var AudioSource = this.gameObject.AddComponent<AudioSource>();
+        sources.Add(AudioSource);
+    }
+    private AudioSource GetAudioSourceFromList()
+    {
+        return sources.Find(x => x.isPlaying == false);
+    }
+    public void PlayClip(AudioClip clip, float volumen, bool loop)
+    {
+        var audioSource = GetAudioSourceFromList();
+        if (audioSource == null) return;
+        audioSource.volume = volumen;
+        audioSource.loop = loop;
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+    public AudioClip GetAudioClip(string clip)
+    {
+        return clips.Find(x => x.name == clip);
     }
 }
