4a3f4bc863e926566cc5f6e7d72314ead3f70daf
Nicolas <nicolas.migues97@gmail.com>
2025-11-30 22:07:41 -0300

    Bell

--- Assets/Scripts/BellEndTurn.cs
+++ Assets/Scripts/BellEndTurn.cs
@@ -4,6 +4,7 @@ public class BellEndTurn : MonoBehaviour
     private void OnMouseDown()
     {
         if (!GameManager.instance.isPlayerTurn) return;
+        SoundManager.Instance.PlayClip(SoundManager.Instance.GetAudioClip("DingSong"), 1f, false);
         GameManager.instance.PlayerRequestsEndTurn();
     }
 }
