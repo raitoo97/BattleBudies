a70746780e341a1d09be65ff2f593b6e2b474bc4
Nicolas <nicolas.migues97@gmail.com>
2025-12-01 08:12:45 -0300

    X

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class TowerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static TowerManager instance;
+    private void Awake()
+    {
+        
+    }
     void Start()
     {
         
--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -1,18 +1,32 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum teamTower
+{
+    player,
+    enemy
+}
 public class Tower : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public teamTower team;
+    public int maxHealth;
+    private int currentHealth;
+    public List<Node> attackableNodes;
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
-
-    // Update is called once per frame
-    void Update()
+    public bool CanBeAttackedFrom(Node node)
     {
-        
+        return attackableNodes.Contains(node);
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            //VictoryManager.instance.TowerDestroyed(this);
+            Destroy(gameObject);
+        }
     }
 }
