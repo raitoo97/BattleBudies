91d9d73c03352c5834ca94af6baf370b16b70cf2
Nicolas <nicolas.migues97@gmail.com>
2025-11-30 21:50:21 -0300

    InitSound

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -7,7 +7,7 @@ public class SoundManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Inicio");
     }
 
     // Update is called once per frame
