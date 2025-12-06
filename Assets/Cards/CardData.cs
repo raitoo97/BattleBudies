b5bc31a82d0017757795861333e132ec2cece528
Nicolas <nicolas.migues97@gmail.com>
2025-12-06 13:02:13 -0300

    okcards

--- Assets/Cards/CardData.cs
+++ Assets/Cards/CardData.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
+public enum UnitType
+{
+    Attacker,
+    Defender,
+    Ranger,
+    None
+}
 [CreateAssetMenu(fileName = "NewCard", menuName = "Cards/Card Data")]
 public class CardData : ScriptableObject
 {
     public Sprite artwork;
     public int cost;          
     public GameObject unitPrefab;
+    public UnitType unitType;
 }
